32e5213527e09a4588c84f4f746ce2cf0d477891
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 16:58:19 -0500

    feat: integre los modelos de carrito y producto con ejemplos en el main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import model.Cart;
+import model.Product;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Product p1 = new Product(1, "Laptop", 2500);
+        Product p2 = new Product(2, "Mouse", 80);
+
+        Cart cart = new Cart();
+
+        cart.addProduct(p1);
+        cart.addProduct(p2);
+
+        System.out.println("Total: S/ " + cart.calculateTotal());
+    }
 }
