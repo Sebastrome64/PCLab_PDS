735ebf9edb4e6277c9d6c9e2eeeadc65e8fc6fd3
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 16:47:29 -0500

    feat: cree el contructor de Product junto a sus getters

---
+++ src/model/Cart.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<Product> products;
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+}
