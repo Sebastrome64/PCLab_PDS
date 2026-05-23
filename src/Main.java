b5b4a36cbca7098b6a87755d50e3268fedf38bbf
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 16:37:47 -0500

    chore: se creo los paquetes en la estructura de el proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,2 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main{
-    public static void main(String[] args) {}
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
 }
