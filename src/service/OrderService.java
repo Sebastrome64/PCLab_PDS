c23c4f6f647be4f0239157e32ebad070cdb0a6f6
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 18:12:31 -0500

    feat: se agrego servicio de pedidos con soporte para observadores

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -4,7 +4,33 @@ import java.util.List;
 import java.util.ArrayList;
 
 import observer.OrderObserver;
+
 public class OrderService {
 
+    private List<OrderObserver> observers;
 
+    public OrderService() {
+
+        observers = new ArrayList<>();
+    }
+
+    public void addObserver(OrderObserver observer) {
+
+        observers.add(observer);
+    }
+
+    public void notifyObservers(String message) {
+
+        for(OrderObserver observer : observers) {
+
+            observer.update(message);
+        }
+    }
+
+    public void confirmOrder(double total) {
+
+        System.out.println("\nCompra confirmada por S/ " + total);
+
+        notifyObservers("Compra realizada");
+    }
 }
