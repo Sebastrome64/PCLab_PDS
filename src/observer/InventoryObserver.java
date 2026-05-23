dc6536ebaceddb24839a44580d92afc9f9baef09
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 18:01:14 -0500

    feat: se implemento el inventario observer

---
+++ src/observer/InventoryObserver.java
@@ -0,0 +1,10 @@
+package observer;
+
+public class InventoryObserver implements OrderObserver{
+
+    @Override
+    public void update(String message) {
+
+        System.out.println("Actualizando inventario...");
+    }
+}
