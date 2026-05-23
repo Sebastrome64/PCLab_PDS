cc5fdfc939a54b367a2c0a2a821877de52f70cd9
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 17:40:55 -0500

    feat: se pago con tarjet y yape como metodos de pago

---
+++ src/adapter/CreditCardPaymentProcessor.java
@@ -0,0 +1,10 @@
+package adapter;
+
+public class CreditCardPaymentProcessor implements PaymentProcessor{
+
+    @Override
+    public void pay(double amount) {
+
+        System.out.println("Pago realiza con tarjeta: S/ " + amount);
+    }
+}
