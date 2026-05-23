6673b4c9068cbad57a4e932fca43c89ba477c719
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 16:52:55 -0500

    feat: se agrego productos al carrito y tambien el recorrido de todos los prodcutos y el for por cada producto dentro de la lista

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -13,4 +13,19 @@ public class Cart {
     public void addProduct(Product product) {
         products.add(product);
     }
+
+    public double calculateTotal() {
+
+        double total = 0;
+
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+
+        return total;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
 }
