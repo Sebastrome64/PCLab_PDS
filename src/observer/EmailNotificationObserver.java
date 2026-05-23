a2f68aa9ebf87ef26ca51fc494becb98dea49826
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-05-23 18:02:38 -0500

    refactor: hubo un error al colocar comillar

--- src/observer/EmailNotificationObserver.java
+++ src/observer/EmailNotificationObserver.java
@@ -5,6 +5,6 @@ public class EmailNotificationObserver implements OrderObserver{
     @Override
     public void update(String message) {
 
-        System.out.println(Enviamos correo al cliente...);
+        System.out.println("Enviamos correo al cliente...");
     }
 }
